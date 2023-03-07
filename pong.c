9f91908a5246d8919d1e549ee3a232fa7911b970
choune <maxride445@gmail.com>
2023-03-07 23:00:01 +0100

    Add refreshing of menu

--- pong.c
+++ pong.c
@@ -9,7 +9,6 @@
 #include "src_ball/ball.c"
 
 
-
 int sound_pid = 0;
 
 
@@ -19,17 +18,15 @@ int main(int argc, char *argv[]) {
     if (argc > 1) {
         if (strcmp(argv[1], "-c") == 0) {
             CONSOLE = true;
-        }
-        else if (strcmp(argv[1], "-d") == 0) {
+        } else if (strcmp(argv[1], "-d") == 0 || strcmp(argv[1], "--debug") == 0) {
             DEBUG = true;
             log_all("Debug mod active\n", LOG_FILE_PATH);
         }
     }
-    if(argc > 2){
+    if (argc > 2) {
         if (strcmp(argv[2], "-c") == 0) {
             CONSOLE = true;
-        }
-        else if (strcmp(argv[2], "-d") == 0) {
+        } else if (strcmp(argv[2], "-d") == 0 || strcmp(argv[2], "--debug") == 0) {
             DEBUG = true;
             log_all("Debug mod active\n", LOG_FILE_PATH);
         }
@@ -58,44 +55,64 @@ int main(int argc, char *argv[]) {
     return 0;
 }
 
-int menu(){
-    logg("Display menu\n", LOG_FILE_PATH);
+int menu() {
+    char frapped_key = '0';
+    int index = 0;
     WINDOW *menu_win;
     WINDOW *title_win;
-    WINDOW *play_btn;
-    WINDOW *settings_btn;
-    WINDOW *credits_btn;
-    WINDOW *exit_btn;
-    WINDOW *debug_win;
-
-
-    initscr();
     // create windows (name, size, position, parent window
-    menu_win= subwin(stdscr, LINES, COLS, 0, 0);
-    title_win= subwin(stdscr, 10, 50, 1, (COLS/2)-25);
-    play_btn= subwin(stdscr, 5, 30, (LINES/5), (COLS/2)-15);
-    settings_btn= subwin(stdscr, 5, 30, (LINES/5)*1.5, (COLS/2)-15);
-    credits_btn= subwin(stdscr, 5, 30, (LINES/5)*2, (COLS/2)-15);
-    exit_btn= subwin(stdscr, 5, 30, (LINES/5)*2.5, (COLS/2)-15);
+    initscr();
+    menu_win = subwin(stdscr, LINES, COLS, 0, 0);
+    set_title(title_win);
+    while (1) {
+        int res = menu_displayer(index, menu_win, title_win);
+        if (res != 0) {
+            log_all("Error : menu failed to start\n", LOG_FILE_PATH);
+            exit(1);
+        }
 
-    if(DEBUG) {
-        debug_win = subwin(stdscr, 10, 30, 1, COLS-31);
-        box(debug_win, ACS_VLINE, ACS_HLINE);
-        mvwprintw(debug_win, 1, 1, "Start debug");
+        scanf("%c", &frapped_key);
+        if(frapped_key == 'w'){
+//            if (index > 0) {
+//                index--;
+//            } else {
+//                index = 3;
+//            }
+            set_title(title_win);
+        }
+        else if(frapped_key == 's'){
+//            if (index < 3) {
+//                index++;
+//            } else {
+//                index = 0;
+//            }
+            clear_title(title_win);
+        }
+        else if(frapped_key == 'q'){
+            break;
+        }
     }
+//    free(menu_win);
+//    free(title_win);
+    endwin();
+    printf("Bye bye\n");
+    return 0;
+}
 
-    // Create border
-    box(menu_win, ACS_VLINE, ACS_HLINE);
-    box(play_btn, ACS_VLINE, ACS_HLINE);
-    box(settings_btn, ACS_VLINE, ACS_HLINE);
-    box(credits_btn, ACS_VLINE, ACS_HLINE);
-    box(exit_btn, ACS_VLINE, ACS_HLINE);
+void clear_menu(WINDOW *menu_win) {
+    menu_win = subwin(stdscr, LINES, COLS, 0, 0);
+    werase(menu_win);
+    wrefresh(menu_win);
+}
 
-    mvwprintw(play_btn, 2, (30/2)-2, "PLAY");
-    mvwprintw(settings_btn, 2, (30/2)-4, "SETTINGS");
-    mvwprintw(credits_btn, 2, (30/2)-4, "CREDITS");
-    mvwprintw(exit_btn, 2, (30/2)-2, "EXIT");
+void clear_title(WINDOW *title_win) {
+    title_win = subwin(stdscr, 10, 50, 1, (COLS / 2) - 25);
+    werase(title_win);
+    wrefresh(title_win);
+}
 
+void set_title(WINDOW *title_win) {
+    title_win = subwin(stdscr, 10, 50, 1, (COLS / 2) - 25);
     mvwprintw(title_win, 1, 2, "8888888b.   .d88888b.  888b    888  .d8888b.  ");
     mvwprintw(title_win, 2, 2, "888   Y88b d88P   Y88b 8888b   888 d88P  Y88b ");
     mvwprintw(title_win, 3, 2, "888    888 888     888 88888b  888 888    888 ");
@@ -104,42 +121,30 @@ int menu(){
     mvwprintw(title_win, 6, 2, "888        888     888 888  Y88888 888    888 ");
     mvwprintw(title_win, 7, 2, "888        Y88b. .d88P 888   Y8888 Y88b  d88P ");
     mvwprintw(title_win, 8, 2, "888          Y88888P   888    Y888  Y8888P88  ");
-
-
-    wrefresh(menu_win);
     wrefresh(title_win);
-    wrefresh(play_btn);
-    wrefresh(settings_btn);
-    wrefresh(credits_btn);
-    wrefresh(exit_btn);
-    if(DEBUG) wrefresh(debug_win);
+}
 
-    int c;
-    while((c = getch()) != 24){
-        if(DEBUG) mvwprintw(debug_win, 1, 1, "Getch value : %d", c);
-        switch(c){
-            case KEY_UP:
-                if(DEBUG) mvwprintw(debug_win, 1, 1, "UP");
-                break;
-            case KEY_DOWN:
-                if(DEBUG) mvwprintw(debug_win, 1, 1,"DOWN");
-                break;
-            default:
-                if(DEBUG) wrefresh(debug_win);
-                break;
-        }
-        refresh();
-    }
-    endwin();
+ void set_play_btn(WINDOW *play_btn,  int index) {
+     play_btn = subwin(stdscr, 5, 30, (LINES / 5), (COLS / 2) - 15);
+     box(play_btn, ACS_VLINE, ACS_HLINE);
+     if(index == 0){
+         mvwprintw(play_btn, 2, (30 / 2) - 2, ">PLAY<");
+     }
+     else{
+         mvwprintw(play_btn, 2, (30 / 2) - 2, "PLAY");
+     }
+     wrefresh(play_btn);
+ }
 
-    free(menu_win);
-    free(title_win);
-    free(play_btn);
-    free(settings_btn);
-    free(credits_btn);
-    free(exit_btn);
-    if(DEBUG) free(debug_win);
+ void clear_play_btn(WINDOW *play_btn) {
+     play_btn = subwin(stdscr, 5, 30, (LINES / 5), (COLS / 2) - 15);
+     werase(play_btn);
+     wrefresh(play_btn);
+ }
 
+int menu_displayer(int index, WINDOW *menu_win, WINDOW *title_win) {
+    logg("Display menu\n", LOG_FILE_PATH);
+    wrefresh(menu_win);
     return 0;
 }
 
--- pong.h
+++ pong.h
@@ -2,6 +2,7 @@
 #define ARCADE_PONG_PONG_H
 
 #include <stdbool.h>
+#include <ncurses.h>
 
 #define ANSI_COLOR_RED     "\x1b[31m"
 #define ANSI_COLOR_GREEN   "\x1b[32m"
@@ -10,6 +11,13 @@
 #define ANSI_COLOR_MAGENTA "\x1b[35m"
 #define ANSI_COLOR_CYAN    "\x1b[36m"
 #define ANSI_COLOR_RESET   "\x1b[0m"
+
+
+#define DOWN 1
+#define UP 2
+#define LEFT 3
+#define RIGHT 4
+
 
 const char *AUTHOR= " ██████╗██╗  ██╗ ██████╗ ██╗   ██╗███╗   ██╗███████╗\n██╔════╝██║  ██║██╔═══██╗██║   ██║████╗  ██║██╔════╝\n██║     ███████║██║   ██║██║   ██║██╔██╗ ██║█████╗\n██║     ██╔══██║██║   ██║██║   ██║██║╚██╗██║██╔══╝\n╚██████╗██║  ██║╚██████╔╝╚██████╔╝██║ ╚████║███████╗\n ╚═════╝╚═╝  ╚═╝ ╚═════╝  ╚═════╝ ╚═╝  ╚═══╝╚══════╝\n";
 const char *TITLE= "8888888b.   .d88888b.  888b    888  .d8888b.  \n888   Y88b d88P   Y88b 8888b   888 d88P  Y88b \n888    888 888     888 88888b  888 888    888 \n888   d88P 888     888 888Y88b 888 888        \n8888888P   888     888 888 Y88b888 888  88888 \n888        888     888 888  Y88888 888    888\n888        Y88b. .d88P 888   Y8888 Y88b  d88P \n888          Y88888P   888    Y888  Y8888P88 \n\n\n";
@@ -29,7 +37,12 @@ int console_menu();
 int console_credits();
 int console_play();
 int console_settings();
-int menu();
+int menu_displayer(int index, WINDOW *menu_win, WINDOW *title_win);
+void set_title(WINDOW *title_win);
+void clear_title(WINDOW *title_win);
+void clear_menu(WINDOW *menu_win);
+void set_play_btn(WINDOW *play_btn, int index);
+void clear_play_btn(WINDOW *play_btn);
 
 
 #endif //ARCADE_PONG_PONG_H
