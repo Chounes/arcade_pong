192606057e24cb8b0ff5960c9e4158ab3c8eeb5c
choune <maxride445@gmail.com>
2023-04-09 23:25:59 +0200

    Add font

--- pong.c
+++ pong.c
@@ -9,18 +9,29 @@
 #include "src_ball/ball.c"
 #include <SDL2/SDL_ttf.h>
 
-#define SCREEN_WIDTH 640
-#define SCREEN_HEIGHT 480
+#define SCREEN_WIDTH (640*1.5)
+#define SCREEN_HEIGHT (480*1.5)
 
-void renderText(SDL_Renderer *renderer, TTF_Font *font, const char *text, SDL_Rect *rect)
-{
-
+void renderText(SDL_Renderer *renderer, TTF_Font *font, const char *text, SDL_Rect *rect){
+    if (text == NULL || rect == NULL) {
+        fprintf(stderr, "Test Erreur lors du rendu du texte : paramètre nul\n");
+        return;
+    }
     SDL_Surface *surface = TTF_RenderText_Solid(font, text, WHITE);
+    if (surface == NULL) {
+        fprintf(stderr, "Erreur lors du rendu du texte : %s\n", TTF_GetError());
+        return;
+    }
     SDL_Texture *texture = SDL_CreateTextureFromSurface(renderer, surface);
     SDL_FreeSurface(surface);
+    if (texture == NULL) {
+        fprintf(stderr, "Erreur lors du rendu du texte : %s\n", SDL_GetError());
+        return;
+    }
     SDL_RenderCopy(renderer, texture, NULL, rect);
     SDL_DestroyTexture(texture);
 }
+
 
 int main(int argc, char *argv[])
 {
@@ -30,7 +41,7 @@ int main(int argc, char *argv[])
     SDL_Window *window = SDL_CreateWindow("SDL Button", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
     SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
 
-    TTF_Font *font = TTF_OpenFont("font.ttf", 24);
+    TTF_Font *font = TTF_OpenFont("./fonts/RussoOne-Regular.ttf", 24);
     SDL_Rect playButtonRect = {SCREEN_WIDTH / 2 - 50, SCREEN_HEIGHT / 7 - 25, 100, 50};
     SDL_Rect settingButtonRect = {SCREEN_WIDTH / 2 - 50, (SCREEN_HEIGHT / 7)*3 - 25, 100, 50};
     SDL_Rect quitButtonRect = {SCREEN_WIDTH / 2 - 50, (SCREEN_HEIGHT / 7)*5 - 25, 100, 50};
@@ -73,9 +84,9 @@ int main(int argc, char *argv[])
         SDL_RenderFillRect(renderer, &settingButtonRect);
         SDL_RenderFillRect(renderer, &quitButtonRect);
 
-        renderText(renderer, font, "PLAY", &playButtonRect);
-        renderText(renderer, font, "SETTING", &settingButtonRect);
-        renderText(renderer, font, "QUIT", &quitButtonRect);
+        renderText(renderer, font, "PLAY\0", &playButtonRect);
+        renderText(renderer, font, "SETTING\0", &settingButtonRect);
+        renderText(renderer, font, "QUIT\0", &quitButtonRect);
 
         SDL_RenderPresent(renderer);
     }
